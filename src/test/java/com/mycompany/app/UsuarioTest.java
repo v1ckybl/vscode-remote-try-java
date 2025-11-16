26c755926dedd40c0d0cf50d82fc66ce636dfcd1
victoria <victoriabladilo@gmail.com>
2025-11-16 18:55:05 -0300

    deja de modificarte aaaaa

--- src/test/java/com/mycompany/app/UsuarioTest.java
+++ src/test/java/com/mycompany/app/UsuarioTest.java
@@ -1,10 +1,10 @@
 package com.mycompany.app;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class UsuarioTest {
